661355f04a9a76d835acf761606875d3bda3cb69
mariaconejo <mary03081@gmail.com>
2020-07-13 12:57:37 -0600

    Se agrego ejercicio 8 chessboard

---
+++ Ejercicio-8/js/script.js
@@ -0,0 +1,19 @@
+let character = ''; 
+let size  = 8;
+
+// El primer for es para las lineas que serian 8
+for(let i = 0; i < size; i++) {
+// El segundo for es para la cantidad de # o '' van por cada fila
+    for(let j = 0; j < size; j++) {
+// El if me indica que si es par entonces coloca un ' ' y si es impar #
+        if( ((i+j) % 2) == 0) {
+            character += ' ';
+        }else {
+            character += '#';
+        } 
+    }
+// salto de linea 
+    character += '\n';
+}  
+// imprime todo 
+console.log(character);
