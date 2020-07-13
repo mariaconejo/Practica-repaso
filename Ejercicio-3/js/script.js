3ee752ccbda59e6254588abac0fddcc97e46fec6
mariaconejo <mary03081@gmail.com>
2020-07-13 09:29:36 -0600

    Se agrego el tercer ejercicio

---
+++ Ejercicio-3/js/script.js
@@ -0,0 +1,5 @@
+const text = prompt('Enter your text').split(''); // split convierte de string a un arreglo
+
+console.log(text);
+text.reverse(); // convierte la palabra al reves
+console.log(text.join(' ')); // convierte de un arreglo a un string
